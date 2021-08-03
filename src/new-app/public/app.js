932b1430bb24a68fce78da6528f8e1d825441649
Dave <david-rowe2@live.nmit.ac.nz>
2021-08-04 09:50:38 +1200

    setup whatever this is

---
+++ src/new-app/public/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+//Set static path
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>about</h1>');
+})
+
+app.get('/users/:name', (req, res) => {
+    let user = req.param.nameres.send()
+    res.send('<h1>'+user+'</h1>');
+})
